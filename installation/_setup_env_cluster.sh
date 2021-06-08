9caa9c7bf5c239c1a16953878770bef47aa464d6
oonisim <oonisim@gmail.com>
2021-06-08 20:47:43 +1000

    Fixed issues observed in Ubuntu 20.04.

--- installation/_setup_env_cluster.sh
+++ installation/_setup_env_cluster.sh
@@ -11,3 +11,5 @@ export TARGET_INVENTORY=${TARGET_INVENTORY:?"Set TARGET_INVENTORY"}
 
 export $(cat ${DIR}/conf/ansible/inventories/${TARGET_INVENTORY}/env/hadoop/env/hadoop_variables.sh | grep -v '^#' | xargs)
 export $(cat ${DIR}/conf/ansible/inventories/${TARGET_INVENTORY}/env/spark/env/spark_variables.sh   | grep -v '^#' | xargs)
+
+set +u
